342f77c991418ebe70e3dea880b145c409596a1d
VikasSastiya <vikassastiya06@gmail.com>
2024-08-25 21:06:39 +0530

    new commit

--- BGChangerProject/bgChanger/src/App.jsx
+++ BGChangerProject/bgChanger/src/App.jsx
@@ -5,13 +5,34 @@ function App() {
   const [color,setColor]=useState('olive');
 
   return (
-    <div className="w-100"> <div className="w-full h-screen duration-200"
+     <div className="fixed w-full h-screen duration-200 "
     style={{backgroundColor:color}}
     >
-    </div>
+      <div className="fixed
+       flex flex-wrap justify-center bottom-12 inster-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+             <button onClick={()=>setColor("red")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{backgroundColor:"red"}}>Red
+             </button>
+             <button onClick={()=>setColor("green")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{backgroundColor:"green"}}>Green
+             </button>
+             <button onClick={()=>setColor("blue")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{backgroundColor:"blue"}}>blue
+             </button>
+             <button onClick={()=>setColor("olive")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{backgroundColor:"olive"}}>olive
+             </button>
+        </div>
+       </div>
       </div>
       
   );
 };
 
 export default App;
+
